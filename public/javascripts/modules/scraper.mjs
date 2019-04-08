59e38bfa154da2d7a7d88974d74627cfadb70464
porpoiseless <darkworldsawake@gmail.com>
2019-04-08 00:39:42 -0700

    Make LexiconScraper handle derived roots properly. Add masthead img.

--- public/javascripts/modules/scraper.mjs
+++ public/javascripts/modules/scraper.mjs
@@ -17,7 +17,8 @@ const nextElementSiblingText = el => nextElementSibling(el) ?
 // get all following text nodes until the next element
 function slurpTextNodes (start) {
     let slurpedText = "", next = nextSibling(start);
-    while (next.nodeType ? next.nodeType == Node.TEXT_NODE : false) {
+    while (next.nodeType &&
+           (next.nodeType == Node.TEXT_NODE || next.nodeName == "EM")) {
         slurpedText += next.textContent;
         next = nextSibling(next)
     }
@@ -39,19 +40,48 @@ const tableToArray = tbl => Array.from(tbl.querySelectorAll("tr")) // table => a
 	    .map(textAndElement));				// cell => {text, element}
 
 // regexp describing a root
-const looseRootRegExp = /-? ?([^0-9: ]{1,6}) ?-?/;
-// requires both hyphens to be present
-const strictRootRegExp = /- ?([^0-9: ]{1,6}) ?-/;
+const laxRootRegExp = /([SŠPTŢKBDGQMNŇJXRŘFVZŻŽLĻÇCČ][WY’hSŠPTŢKBDGQMNŇJXRŘFVZŻŽLĻÇCČ]*)/;
+const strictRootRegexp =  /[\-–]([SŠPTŢKBDGQMNŇJXRŘFVZŻŽLĻÇCČ][WY’hʰHSŠPTŢKBDGQMNŇJXRŘFVZŻŽLĻÇCČ]*)\b/;
+
 // derived roots aren't children of a table
 const derivedRootFilter = el => ! el.closest("table");
+
 // all derived root labels
 const getAllDerivedRoots = doc => Array.from(doc.querySelectorAll("strong"))
       .filter(derivedRootFilter);
-const isolateDerivedRoot = el => looseRootRegExp.test(getText(el)) ?
-      getText(el).match(looseRootRegExp)[1] : false;
+// 
+const isolateDerivedRoot = el => getText(el).match(/\//) ? false : getText(el).replace(/[–\-\ ]/g,"");
+// 
+const cleanGloss = gloss => gloss.replace(/^\W*|\W*$/g, "") // remove quotes etc.
+      .replace(/\s\s+/g, " ");			// remove extraneous space
 // extract & parse derived roots
 const extractDerivedRoot = el => ({root: isolateDerivedRoot(el),
-                                   gloss: slurpTextNodes(el)});
+                                   gloss: cleanGloss(slurpTextNodes(el)),
+                                   next: nextElementSiblingText(el)});
+
+// best guess at major root, mark as orphan if no guess can be made
+const guessMajorRoot = ({root, gloss, next}) => {
+    if (next && laxRootRegExp.test(next)) {
+        return {root: root, gloss: gloss, major: next.match(laxRootRegExp)[1]};
+    } else if (gloss && strictRootRegexp.test(gloss)) {
+        return {root: root, gloss: gloss, major: gloss.match(strictRootRegexp)[1]};
+    } else {
+        return {root: root, gloss: gloss, major: false, attempted: next};
+    }
+};
+
+const derivedRootReducer = (acc={index:{},orphans:[]}, item) => {
+    const index=acc.index, orphans=acc.orphans, major=item.major;
+    if (item.root && major) {    // if we have a candidate for the derived and major roots 
+        const newMajorIndex = index.hasOwnProperty(major) ? // add it to the existing entry
+              index[major].concat(item) : Array.of(item); // or create a new one
+        const newIndex = {...index, [major]: newMajorIndex };
+        return {...acc, index: newIndex};
+    } else {
+        const newOrphans = orphans.concat(item);
+        return {...acc, orphans: newOrphans};
+    }
+};
 
 class LexiconScraper {
     constructor (page) {
@@ -65,7 +95,9 @@ class LexiconScraper {
                           node: tbl}));
     }
     static derivedRoots (page) {
-        return getAllDerivedRoots(page).map(extractDerivedRoot);
+        return getAllDerivedRoots(page).map(extractDerivedRoot)
+            .map(guessMajorRoot)
+            .reduce(derivedRootReducer, {index:{}, orphans:[]});
     }
 }
 // inject 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Ithkuil Dictionary',
+			  subtitle: 'An interactive listing of Ithkuil roots, stems, and derivatives.',
+			  masthead: "/images/masthead.svg" });
 });
 
 module.exports = router;
